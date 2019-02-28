6a22ba01a123b77fc62897742a91a5b6200a9127
Monnyka <31350922+Monnyka@users.noreply.github.com>
2019-02-28 23:14:33 +0700

    -Update Register Activity

--- app/src/main/java/com/nyka/together/Log_in_Activity.java
+++ app/src/main/java/com/nyka/together/Log_in_Activity.java
@@ -1,13 +1,33 @@
 package com.nyka.together;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Log_in_Activity extends AppCompatActivity {
+
+    private Button mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_);
+        mRegister=(Button) findViewById(R.id.btnStart);
+
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    private void openRegisterActivity(){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/nyka/together/RegisterActivity.java
+++ app/src/main/java/com/nyka/together/RegisterActivity.java
@@ -1,13 +1,31 @@
 package com.nyka.together;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private ImageButton mBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mBackButton=(ImageButton) findViewById(R.id.btnBack);
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
     }
+
+    private void openLoginActivity(){
+        Intent intent =new Intent(this,Log_in_Activity.class);
+        startActivity(intent);
+    }
+
 }
