5064cfc5b9b31a0de7d7b4c7b1dc350781c01fab
Monnyka <31350922+Monnyka@users.noreply.github.com>
2019-03-02 01:37:34 +0700

    -Add Chat Activity -Update UI

--- app/src/main/java/com/nyka/together/Log_in_Activity.java
+++ app/src/main/java/com/nyka/together/Log_in_Activity.java
@@ -1,33 +1,53 @@
 package com.nyka.together;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Log_in_Activity extends AppCompatActivity {
 
     private Button mRegister;
+    private TextView mRegisterNow;
+    private EditText mEdtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_);
         mRegister=(Button) findViewById(R.id.btnStart);
+        mRegisterNow=(TextView)findViewById(R.id.txtRegister);
+        mEdtEmail=(EditText)findViewById(R.id.edtEmail);
 
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                openChatActivity();
+            }
+        });
+        mRegisterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 openRegisterActivity();
             }
         });
+
+
     }
 
+    //Call Register Activity
     private void openRegisterActivity(){
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
     }
+    private  void openChatActivity(){
+        Intent intent = new Intent(this,activity_chat.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/nyka/together/custom_message_alert.java
@@ -0,0 +1,13 @@
+package com.nyka.together;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class custom_message_alert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_message_alert);
+    }
+}
